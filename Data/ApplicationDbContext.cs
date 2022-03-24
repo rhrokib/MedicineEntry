620a97c61f1d81c2c5865e39d6084c5392ad8836
Rokibul Islam <rokib.hridoy@gmail.com>
2022-03-25 01:12:20 +0600

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MedicineEntry.Models;
+
+namespace MedicineEntry.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MedicineEntry.Models.Medicine> Medicine { get; set; }
+    }
+}
---
+++ Models/Medicine.cs
@@ -0,0 +1,13 @@
+﻿namespace MedicineEntry.Models
+{
+    public class Medicine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+
+        public Medicine()
+        {
+        }
+    }
+}
